7a6c482fb223fabaa7cf9b42029aa256578d93b9
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-08-16 22:07:15 +0200

    Create DB seed data, populate DB on server start

---
+++ seed/seed.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+const { ObjectID } = require("mongodb");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/user");
+
+const ids = [];
+
+for (let index = 0; index < 4; index++) {
+  ids.push(new ObjectID());
+}
+
+const hashPassword = password => {
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt);
+  return hash;
+};
+
+const users = [
+  {
+    _id: ids[0],
+    username: "Jethro",
+    email: "jethro@gmail.com",
+    password: hashPassword("password1"),
+    token: jwt.sign({ id: ids[0] }, process.env.SECRET_KEY)
+  },
+  {
+    _id: ids[1],
+    username: "NariRoh",
+    email: "nariroh@gmail.com",
+    password: hashPassword("password2"),
+    token: jwt.sign({ id: ids[1] }, process.env.SECRET_KEY)
+  },
+  {
+    _id: ids[2],
+    username: "JonMaldia",
+    email: "jonmaldia@gmail.com",
+    password: hashPassword("password3"),
+    token: jwt.sign({ id: ids[2] }, process.env.SECRET_KEY)
+  },
+  {
+    _id: ids[3],
+    username: "Alexever",
+    email: "alexever@gmail.com",
+    password: hashPassword("password4"),
+    token: jwt.sign({ id: ids[3] }, process.env.SECRET_KEY)
+  }
+];
+
+const populateUsers = () => {
+  User.remove({})
+    .then(() => {
+      return User.insertMany(users);
+    })
+    .then(() => console.log("DB populated"))
+    .catch(err => console.log(err));
+};
+
+module.exports = populateUsers;
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const populateUsers = require("./seed/seed");
+
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -10,4 +12,5 @@ app.use(bodyParser.json());
 
 app.listen(port, () => {
   require("./db/mongoose");
+  populateUsers();
 });
