47890102ac62dc752f9a0581f80cf513d36d3d52
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-08-22 20:30:29 +0200

    Remove `hashPassword()`, refactor `populateUsers()`

--- seed/seed.js
+++ seed/seed.js
@@ -1,9 +1,9 @@
-const mongoose = require("mongoose");
-const { ObjectID } = require("mongodb");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const mongoose = require('mongoose');
+const { ObjectID } = require('mongodb');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const User = require("../models/user");
+const User = require('../models/user');
 
 const ids = [];
 
@@ -11,54 +11,51 @@ for (let index = 0; index < 4; index++) {
   ids.push(new ObjectID());
 }
 
-const hashPassword = password => {
-  const salt = bcrypt.genSaltSync(10);
-  const hash = bcrypt.hashSync(password, salt);
-  return hash;
-};
-
 const users = [
   {
     _id: ids[0],
-    username: "Jethro",
-    email: "jethro@gmail.com",
-    password: hashPassword("password1"),
+    username: 'Jethro',
+    email: 'jethro@gmail.com',
+    password: 'password1',
     token: jwt.sign({ id: ids[0] }, process.env.JWT_SECRET)
     // token: null
   },
   {
     _id: ids[1],
-    username: "NariRoh",
-    email: "nariroh@gmail.com",
-    password: hashPassword("password2"),
+    username: 'NariRoh',
+    email: 'nariroh@gmail.com',
+    password: 'password2',
     // token: jwt.sign({ id: ids[1] }, process.env.JWT_SECRET)
     token: null
   },
   {
     _id: ids[2],
-    username: "JonMaldia",
-    email: "jonmaldia@gmail.com",
-    password: hashPassword("password3"),
+    username: 'JonMaldia',
+    email: 'jonmaldia@gmail.com',
+    password: 'password3',
     token: jwt.sign({ id: ids[2] }, process.env.JWT_SECRET)
     // token: null
   },
   {
     _id: ids[3],
-    username: "Alexever",
-    email: "alexever@gmail.com",
-    password: hashPassword("password4"),
+    username: 'Alexever',
+    email: 'alexever@gmail.com',
+    password: 'password4',
     token: jwt.sign({ id: ids[3] }, process.env.JWT_SECRET)
     // token: null
   }
 ];
 
-const populateUsers = () => {
+const populateUsers = done => {
   User.remove({})
     .then(() => {
-      return User.insertMany(users);
+      return User.create(users);
     })
-    .then(() => console.log("DB populated"))
+    .then(() => {
+      if (done) done();
+      console.log('DB populated');
+    })
     .catch(err => console.log(err));
 };
 
-module.exports = populateUsers;
+module.exports = { populateUsers, users };
