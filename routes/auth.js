1ffd62cdcced2990c5660585049e3ff02a8a2e53
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-08-22 20:31:00 +0200

    Remove call to `generateAuthToken()`

--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,21 @@
-const express = require("express");
 const _ = require('lodash');
 
 const User = require('../models/user');
 
 module.exports = express => {
-    const router = express.Router();
-    
-    router.post('/login', (req, res) => {
-        const body = _.pick(req.body, ["email", "password"]);
+  const router = express.Router();
 
-        User.findByCredentials(body.email, body.password).then(user => {
-            return user.generateAuthToken().then(token => {
-                res.header('x-auth', token).send();
-            });
-        }).catch(err => {
-            res.status(400).send(err);
-        });
-    });
+  router.post('/login', (req, res) => {
+    const body = _.pick(req.body, ['email', 'password']);
 
-    return router;
+    User.findByCredentials(body.email, body.password)
+      .then(user => {
+        res.header('x-auth', user.token).send();
+      })
+      .catch(err => {
+        res.status(400).send(err);
+      });
+  });
+
+  return router;
 };
