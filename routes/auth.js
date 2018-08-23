8beb0440a932c17d1146b42c8ecbfc82f4c11e48
Jon Maldia <jon@maldia.net>
2018-08-23 00:04:13 -0400

    Add ability to add save email and password to DB

--- routes/auth.js
+++ routes/auth.js
@@ -7,9 +7,17 @@ module.exports = express => {
     const router = express.Router();
     
     router.post('/register', (req, res) => {
+        let user = new User();
         const body = _.pick(req.body, ["email", "password"]);
 
-    
+        // When someone registers:
+        // TODO: Hash the password
+        // TODO: Create a token
+        // TODO: Add the user to the db  
+
+        user.email = body.email;
+        user.password = body.password;
+        
     });
     
     return router;
