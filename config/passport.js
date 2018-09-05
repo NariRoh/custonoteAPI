5b995eda11f4b074650b8c48d0500b517da5e5d8
Nari Roh <nari.rohh@gmail.com>
2018-09-05 16:06:33 -0500

    Add configuration for google OAuth

--- config/passport.js
+++ config/passport.js
@@ -1,45 +1,76 @@
-const passport = require("passport");
+const passport = require('passport');
 const passportJWT = require('passport-jwt');
-const GitHubStrategy = require("passport-github2").Strategy;
+const GitHubStrategy = require('passport-github2').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-const User = require("../models/user");
+const User = require('../models/user');
 
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 
 passport.use(
-    new GitHubStrategy(
-        {
-            clientID: GITHUB_CLIENT_ID,
-            clientSecret: GITHUB_CLIENT_SECRET,
-            callbackURL: "/auth/github/redirect",
-        },
-        (accessToken, refreshToken, profile, done) => {
-            process.nextTick(() => {
-                User.findOne({ email: profile.emails[0].value })
-                    .then(existingUser => {
-                        if (existingUser) {
-                            console.log("user found");
-                            return done(null, existingUser);
-                        }
+    new GitHubStrategy({
+        clientID: GITHUB_CLIENT_ID,
+        clientSecret: GITHUB_CLIENT_SECRET,
+        callbackURL: '/auth/github/redirect'
+    }, (accessToken, refreshToken, profile, done) => {
+        process.nextTick(() => {
+            User.findOne({ email: profile.emails[0].value })
+                .then(existingUser => {
+                    if (existingUser) {
+                        console.log('user found');
+                        return done(null, existingUser);
+                    }
 
-                        new User({
-                            username: profile.displayName,
-                            email: profile.emails[0].value,
-                            'github.githubID': profile.id
-                        })
-                            .generateAuthToken()
-                            .then(newUser => {
-                                console.log("created new user", newUser);
-                                done(null, newUser);
-                            })
+                    new User({
+                        username: profile.displayName,
+                        email: profile.emails[0].value,
+                        'github.githubID': profile.id
                     })
-                    .catch(err => console.log(err));
-            });
-        }
-    )
+                        .generateAuthToken()
+                        .then(newUser => {
+                            console.log('created new user', newUser);
+                            done(null, newUser);
+                        });
+                })
+                .catch(err => console.log(err));
+        });
+    })
 );
 
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
+passport.use(
+    new GoogleStrategy({
+        clientID: GOOGLE_CLIENT_ID,
+        clientSecret: GOOGLE_CLIENT_SECRET,
+        callbackURL: '/auth/google/redirect'
+    }, (accessToken, refreshToken, profile, done) => {
+        // console.log(profile);
+
+        User.findOne({ email: profile.emails[0].value})
+            .then(existingUser => {
+                if (existingUser) {
+                    console.log('user found');
+                    return done(null, existingUser);
+                }
+
+                new User({
+                    username: profile.displayName,
+                    email: profile.emails[0].value,
+                    thumbnail: profile.photos[0].value,
+                    'google.googleID': profile.id
+                })
+                    .generateAuthToken()
+                    .then(newUser => {
+                        console.log('created new user', newUser);
+                        done(null, newUser);
+                    });
+            })
+            .catch(err => console.log(err));
+    }
+));
 
 const ExtractJwt = passportJWT.ExtractJwt;
 const JWTStrategy = passportJWT.Strategy;
@@ -63,5 +94,5 @@ const JwtStrategy = new JWTStrategy(options, (payload, next) => {
 });
 
 module.exports = {
-  JwtStrategy
+    JwtStrategy
 };
