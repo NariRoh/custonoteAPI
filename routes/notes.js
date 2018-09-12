3daee74ebffb642741be4b1ed23aafc1151aff91
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-09-12 21:03:39 +0200

    Parse errors before returning them

--- routes/notes.js
+++ routes/notes.js
@@ -1,24 +1,27 @@
 const router = require('express').Router();
 
 const User = require('../models/user');
+const { DBErrorParser } = require('../utils/utils');
 
 router.post('/create', (req, res) => {
-    const { user, body } = req;
-    const note = {
-        heading: body.heading,
-        body: body.body
-    };
+  const { user, body } = req;
+  const note = {
+    heading: body.heading,
+    body: body.body
+  };
 
-    user.notes.push(note);
-    user.save()
-        .then(userObj => {
-            // reverse the order of the notes and send the latest one (created one)
-            userObj.notes.reverse();
-            res.json(userObj.notes[0]);
-        })
-        .catch(err => {
-            res.status(400).send(err)
-        });
+  user.notes.push(note);
+  user
+    .save()
+    .then(userObj => {
+      // reverse the order of the notes and send the latest one (created one)
+      userObj.notes.reverse();
+      res.json(userObj.notes[0]);
+    })
+    .catch(err => {
+      const errors = DBErrorParser(err);
+      res.status(400).send(errors);
+    });
 });
 
 module.exports = router;
